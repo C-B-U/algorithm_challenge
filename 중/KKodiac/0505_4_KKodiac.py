b6b4b88fa935ad929b96faf0242b9ea9eacdc222
Sean Hong(홍성민) <35219323+KKodiac@users.noreply.github.com>
2022-05-05 18:44:24 +0900

    <4일차 문제> (#29)
    * Add solution for  <1일차 문제>(벌집)(#2)- KKodiac
    
    * Add solution for  <2일차 문제>(Base Conversion)(#8)- KKodiac
    
    * Add solution for <3일차 문제:사탕게임> - KKodiac
    
    * Add solution for <4일차 문제: 연속합>
    
    * Fix typo

---
+++ 중/KKodiac/0505_4_KKodiac.py
@@ -0,0 +1,14 @@
+def solution():
+    """ 수열의 i번째까지의 연속된 값들의 합과 수열의 i번째 인덱스에서 나온 값 중 큰 놈을 고르면 됨 """
+    n = int(input())
+    sequences = list(map(int, input().split()))
+    table = [sequences[0]]
+
+    for i in range(n-1):
+        table.append(max(table[i]+sequences[i+1], sequences[i+1]))
+
+    print(max(table))
+
+
+if '__main__' == __name__:
+    solution()
