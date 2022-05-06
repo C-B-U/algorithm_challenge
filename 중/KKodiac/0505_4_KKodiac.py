c707e584d8491da294e464d1f073dfb406c02cd1
Sean Hong(홍성민) <seanhong2000@gmail.com>
2022-05-07 00:31:44 +0900

    Update solution <4일차 연속합> using Kadane's Algorithm

--- 중/KKodiac/0505_4_KKodiac.py
+++ 중/KKodiac/0505_4_KKodiac.py
@@ -1,5 +1,6 @@
 def solution():
     """ 수열의 i번째까지의 연속된 값들의 합과 수열의 i번째 인덱스에서 나온 값 중 큰 놈을 고르면 됨 """
+    """ DP 방식으로 생각하는 것 """
     n = int(input())
     sequences = list(map(int, input().split()))
     table = [sequences[0]]
@@ -9,6 +10,15 @@ def solution():
 
     print(max(table))
 
+    """ Kadane's Algorithm : 
+    현재까지의 합이 양수라면: 앞으로 더 값이 커질 것이므로 해당 값을 Keep.
+    만약 음수라면 다음 합한 값은 무조건 여태 합한 값보다 작아지니 합하는 것을 멈추고 다음 Loop.
+    """
+
+    for i in range(1, n):
+        if sequences[i-1] > 0:
+            sequences[i] += sequences[i-1]
+    print(max(sequences))
 
 if '__main__' == __name__:
     solution()
