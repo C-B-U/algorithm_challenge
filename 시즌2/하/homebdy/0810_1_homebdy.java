178702a6e2e641b5abeefc88f3580c88a0a91c2b
homebdy <love_homebody@naver.com>
2022-08-11 02:10:41 +0900

    Create 0810_2_homebdy

--- 시즌2/하/homebdy/0810_1_homebdy.java
+++ 시즌2/하/homebdy/0810_1_homebdy.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class homebdy_1 {
+class homebdy_1 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int num = scanner.nextInt();
