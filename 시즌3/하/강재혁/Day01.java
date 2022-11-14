f9b14ff0a49c699555f882ff4eb8517729379006
ggamD00 <jaehuek@naver.com>
2022-11-14 19:09:12 +0900

    시즌3_하_Day01_강재혁

---
+++ 시즌3/하/강재혁/Day01.java
@@ -0,0 +1,40 @@
+package 강재혁;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
+public class Day01 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        List<Integer> white = new ArrayList<>();
+        List<Integer> black = new ArrayList<>();
+
+        white.add(sc.nextInt());
+        black.add(sc.nextInt());
+        sc.nextLine();
+        black.add(sc.nextInt());
+        white.add(sc.nextInt());
+        sc.nextLine();
+        white.add(sc.nextInt());
+        black.add(sc.nextInt());
+
+        int wMin= Collections.min(white);
+        int bMin= Collections.min(black);
+
+        int min=0;
+        if(wMin<bMin){
+            min=wMin;
+        } else {
+            min=bMin;
+        }
+
+        if (wMin != bMin) {
+            System.out.println(min*2+1);
+        } else {
+            System.out.println(min*2);
+        }
+    }
+}
