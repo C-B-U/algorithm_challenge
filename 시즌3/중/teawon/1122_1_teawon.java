b4324859b7e76f017e52daa539cc90930354e3cf
teawon <xodnjsvkvk9@gmail.com>
2022-11-23 01:16:36 +0900

    Revert "시즌3 8일차_중_정태원"
    This reverts commit 3558ddfc8ced941265afb0398e18cf8dffaa50cb.

--- 시즌3/중/teawon/1122_1_teawon.java
+++ 시즌3/중/teawon/1122_1_teawon.java
@@ -1,54 +1,43 @@
+
 import java.util.*;
 import java.io.*;
+
 /*
- * 문제 주소 :  https://www.acmicpc.net/problem/1920
- **/
+ * 문제 주소 :  https://www.acmicpc.net/problem/1449
+ * 14228KB | 136ms
+ * 
+*/
 
 public class Main {
 
-    static int[] arr;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-        StringBuffer sb = new StringBuffer();
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int N = Integer.parseInt(br.readLine()); // N개의 정수
-        arr = new int[N];
+        int N = Integer.parseInt(st.nextToken()); // 물이 새는 개수
+        int L = Integer.parseInt(st.nextToken()); // 테이프의 길이
+
+        boolean[] leak_place = new boolean[1002];
+
+        int count = 0; // 테이프의 개수
+
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
-        }
-        Arrays.sort(arr);
-
-        int M = Integer.parseInt(br.readLine()); // M개의 정수
-
-        st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < M; i++) {
-            int target = Integer.parseInt(st.nextToken());
-            sb.append(binary_search(target, 0, N - 1) + "\n");
-
+            int index = Integer.parseInt(st.nextToken());
+            leak_place[index] = true;
         }
 
-        System.out.print(sb);
+        for (int i = 1; i < 1002; i++) {
+            if (leak_place[i]) { // 물이 샌다면 해당 지점 막는다.
+                count++;
+                for (int l = 0; l < L; l++) {
+                    if (i + l < 1002)
+                        leak_place[i + l] = false;
+                }
+            }
+        }
 
+        System.out.println(count);
     }
 
-    public static int binary_search(int target, int start, int end) {
-
-        if (start > end) {
-            return 0;
-        }
-
-        int mid = (start + end) / 2;
-
-        if (arr[mid] < target) {
-            return binary_search(target, mid + 1, end);
-        } else if (arr[mid] > target) {
-            return binary_search(target, start, mid - 1);
-        } else {
-            return 1;
-        }
-
-    }
 }
