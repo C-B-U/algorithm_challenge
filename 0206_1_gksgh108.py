4a7c3c4ef78c6477560e51f1d36709fd92de120b
Hanho Kim <114083614+gksgh108@users.noreply.github.com>
2023-02-06 20:48:47 +0900

    Create 0206_1_gksgh108.py

---
+++ 0206_1_gksgh108.py
@@ -0,0 +1,14 @@
+N = int(input())
+cnt = 0
+start = N
+while 1:
+    a = start//10
+    b = start%10
+    c = (a+b)%10
+    start = (b*10) + c
+    cnt += 1
+
+    if(start == N):
+        break
+
+print(cnt)
