2e67031bb4e0c17f3258fb4b1dd53417ccdb2ff6
shay0454 <108744131+shay0454@users.noreply.github.com>
2023-02-10 02:57:31 +0900

    0210_5_shay0454
    get() 사용으로 인한 c++17 이 아닌 c99 사용

---
+++ 시즌4/하/shay0454/0210_5_shay0454.c
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+int main() {
+	char *a= (char*)malloc(1000001),*x;
+	gets(a);
+	int count = 0;
+	while (a!=NULL) {
+		if (a[0] != ' '&&a[0]!='\0') count++;
+		a = strstr(a, " ");
+		if (a == NULL) break;
+		else if (a[0] == ' ') a++;
+	}
+	printf("%d", count);
+	return 0;
+}
