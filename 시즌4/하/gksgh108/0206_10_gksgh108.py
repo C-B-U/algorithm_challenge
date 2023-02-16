6bb836a303376bdf2f812610e9dac1a3835f9e70
Hanho Kim <114083614+gksgh108@users.noreply.github.com>
2023-02-16 20:21:33 +0900

    Update 0206_10_gksgh108.py

--- 시즌4/하/gksgh108/0206_10_gksgh108.py
+++ 시즌4/하/gksgh108/0206_10_gksgh108.py
@@ -11,3 +11,17 @@ for _ in range(K):
         for col in range(j-1, y):
             result += array[row][col]
     print(result)
+
+# N, M = map(int, input().split())
+# arr = [list(map(int, input().split())) for _ in range(N)]
+
+# K = int(input())
+# dp = [[0] * (M+1) for _ in range(N+1)]
+
+# for i in range(1, N+1):
+#    for j in range(1, M+1):
+#        dp[i][j] = arr[i-1][j-1] + dp[i][j-1] + dp[i-1][j] - dp[i-1][j-1]
+
+#for _ in range(K):
+#    i, j, x, y = map(int, input().split())
+#    print(dp[x][y] - dp[x][j-1] - dp[i-1][y] + dp[i-1][j-1])
