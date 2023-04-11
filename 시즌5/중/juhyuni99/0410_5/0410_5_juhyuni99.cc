3e8ab9f26cf3f4145224501eda947e0f43427a4e
juhyuni99 <102004234+juhyuni99@users.noreply.github.com>
2023-04-11 21:45:25 +0900

    Update 0410_5_juhyuni99.cc

--- 시즌5/중/juhyuni99/0410_5/0410_5_juhyuni99.cc
+++ 시즌5/중/juhyuni99/0410_5/0410_5_juhyuni99.cc
@@ -1,31 +1,22 @@
 #include <iostream>
 #include <algorithm>
-#include <vector>
-#include <cmath>
 using namespace std;
 
 
-int t, b; // 타워버거, 불고기버거 개수
-int cola;// 콜라 마신 시간
-int result; // 먹은 햄버거 개수
 
 int main(void) {
-	ios::sync_with_stdio(false);
-	cin.tie(0);
-	cout.tie(0);
 	int t_n, b_m; // 타워버거, 불고기버거 걸리는 시간
 	int T; // 먹을 수 있는 시간
-	int result = 0; // 먹은 햄버거 개수
 	cin >> t_n >> b_m >> T;
-	int r[1000]; 
-	for (t = T / t_n; t > 0; t--) {
-		for (b = 0; b <= T/b_m; b++) {
-			if ((t_n * t) + (b_m * b) > T) continue;
-			cola = T - (t_n * t) + (b * b_m);
-			r[t+b] = { cola };
-			result = max(result, t + b);
+	int r2 = T;
+	int r1 = 0;
+	for (int i = 0; t_n * i <= T; i++) {
+		int k = T - t_n * i;
+		int p = k / b_m;
+		int q = k % b_m;
+		if (q <= r2) {
+			q == r2 ? (r1 = max(r1, i + p)) : (r1 = i + p, r2 = q);
 		}
 	}
-	
-	cout << result << " " << r[result] << '\n';
+	cout << r1 << ' ' << r2;
 }
