a38dac12f7ef0326aaafe4921282b4ebd40d3e49
mr6208 <93671010+mr6208@users.noreply.github.com>
2023-08-07 16:03:36 +0900

    (하) mr6208

--- 시즌6/mr6208/0807_1_mr6208.java
+++ 시즌6/mr6208/0807_1_mr6208.java
@@ -1,1 +1,29 @@
+import java.util.*;
+import java.io.*;
 
+public class Main {
+
+	public int solution (int[] arr) {
+		int answer = 0;
+		int sum = 0;
+		for(int i = 0; i < arr.length; i++){
+			int tmp = arr[i] * arr[i];
+			sum += tmp;
+		}
+		answer = sum%10;
+		return answer;
+}
+
+
+    public static void main(String[] args) throws IOException {
+		Scanner kb = new Scanner(System.in);
+		Main T = new Main();
+		int arr[] = new int[5];
+		for(int i = 0; i < 5; i++){
+			arr[i] = kb.nextInt();
+		}
+
+		System.out.println(T.solution(arr));
+		
+	}
+}
