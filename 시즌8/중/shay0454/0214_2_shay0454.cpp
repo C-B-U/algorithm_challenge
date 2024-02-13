4cc8363603bf204b1e4797488a52b78920328d15
shay0454 <108744131+shay0454@users.noreply.github.com>
2024-02-14 00:55:05 +0900

    Update 0214_2_shay0454.cpp

--- 시즌8/중/shay0454/0214_2_shay0454.cpp
+++ 시즌8/중/shay0454/0214_2_shay0454.cpp
@@ -5,7 +5,7 @@ int get_num_of_wrong_alphabet(string a,string b, int gab) {
 	for (int i = 0; i < a.size(); i++)if (a[i] != b[i + gab])diff++;
 	return diff;
 }
-int check_diff(string a,string b){
+int check_min_diff(string a,string b){
 	int min = 50;
 	for (int i = 0, temp; i <= b.size() - a.size(); i++) { if (min > (temp = get_num_of_wrong_alphabet(a, b, i))) min = temp; }
 	return min;
@@ -14,7 +14,7 @@ int main() {
 	ios::sync_with_stdio(0); cin.tie(0);
 	string a, b;
 	cin >> a >> b;
-	cout << check_diff(a, b);
+	cout << check_min_diff(a, b);
 	//임의의 알파벳을 앞, 뒤로 넣으므로 임의의 알파벳을 겹치는 알파벳을 넣었다고 보게 될 수 있음
 	//이 경우 원래의 string a와 a의 앞에 넣은 gab에 해당하는 b의 위치부터 비교만 하면 됨 (앞은 맞게 넣었고, 뒤는 차이가 있어도 같은 알파벳을 넣어 차이가 없게 할 수 있음)
 }
