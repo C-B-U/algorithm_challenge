f1d5d155dea029c34b1782840b3c37bf6b4cb631
kimkimgungunwoo <gunwoo7179@gmail.com>
2024-04-01 21:23:20 +0900

    0401kimkimgungunwoo

--- 시즌9/상/kimkimgungunwoo/0401_kimkimgungunwoo.py
+++ 시즌9/상/kimkimgungunwoo/0401_kimkimgungunwoo.py
@@ -14,8 +14,6 @@ for i in range(1,m+1):
 answer=0
 for i in range(1,m+1):
     for j in range(1,n+1):
-        answer=max(field[i][j],answer)
-        
-        
+        answer=max(field[i][j],answer)      
 print(answer**2)
             
