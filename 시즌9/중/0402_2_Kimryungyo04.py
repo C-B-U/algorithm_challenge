b272596f6c4a8366427f3d990bd05b37bb4006c3
Kimryungyo04 <165171093+Kimryungyo04@users.noreply.github.com>
2024-04-02 01:26:51 +0900

    Update 0402_2_Kimryungyo04.py

--- 시즌9/중/0402_2_Kimryungyo04.py
+++ 시즌9/중/0402_2_Kimryungyo04.py
@@ -13,7 +13,6 @@
     # 더슨 크릭 --------- 1422 Miles --------->  델타 정션  --------- 1422 Miles ---------> 더슨 크릭
     # ARR -----------------------------------> DES ARR -----------------------------------> DES
 
-
 def main():
     """씨부엉 난이도 중 2일차 알고리즘 함수"""
 
@@ -29,16 +28,17 @@ def main():
         if charging_station_count == 0: break
 
         # 충전소 위치 입력
+        # 델타 정션 이후의 부분은 대칭이므로 계산 할 이유가 없으니 델타 정션 이후의 첫 충전소까지만 입력
         charging_stations = []
         for i in range(charging_station_count): 
             location = int(input())
             charging_stations.append(location)
-            charging_stations.append(highway_length - location)
         charging_stations.sort()
+        charging_stations.append(highway_length - charging_stations[-1])
 
         # 충전소 사이의 거리가 200을 초과하는 경우가 있으면 결과를 IMPOSSIBLE으로 지정, 없으면 POSSIBLE으로 지정
         arrival_possible = "POSSIBLE"
-        for i in range(len(charging_stations) - 1):
+        for i in range(charging_station_count):
             if charging_stations[i + 1] - charging_stations[i] > 200: 
                 arrival_possible = "IMPOSSIBLE"
                 break
