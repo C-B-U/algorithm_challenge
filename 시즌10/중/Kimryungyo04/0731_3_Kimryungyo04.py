ba693f72b3e98bc55996acc8c4c14635b1c47592
Kimryungyo04 <165171093+Kimryungyo04@users.noreply.github.com>
2024-07-31 00:08:24 +0900

    Create 0731_3_Kimryungyo04.py

---
+++ 시즌10/중/Kimryungyo04/0731_3_Kimryungyo04.py
@@ -0,0 +1,23 @@
+# 브루트포스로 체크
+
+n = int(input())
+if n < 10: print(n); quit()
+count = 9
+
+for num in range(10, n + 1):
+    
+    str_num = str(num)
+    common_diff = int(str_num[1]) - int(str_num[0])
+
+    count += 1
+    before = int(str_num[0]) - common_diff
+
+    for sub_num in str_num:
+        sub_num = int(sub_num)
+        if sub_num != (before + common_diff):
+            is_arith = False
+            count -= 1
+            break
+        before = sub_num
+
+print(count)
