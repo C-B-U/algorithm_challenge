4743f3e192bee38437181364fb9f82a182aa4336
Kimryungyo04 <165171093+Kimryungyo04@users.noreply.github.com>
2024-08-01 00:01:34 +0900

    Create 0801_4_Kimryungyo04.py

---
+++ 시즌10/중/Kimryungyo04/0801_4_Kimryungyo04.py
@@ -0,0 +1,10 @@
+dp = [[1, 0], [0, 1]]
+
+def fibonacci(n):
+    for _ in range(n - len(dp) + 1):
+        dp.append([dp[-1][0] + dp[-2][0], dp[-1][1] + dp[-2][1]])
+
+    return dp[n]
+
+for _ in range(int(input())):
+    print(*fibonacci(int(input())))
