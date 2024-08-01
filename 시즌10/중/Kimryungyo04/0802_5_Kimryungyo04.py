343fc7a02d5fcc0fc90b46b1cd4fe94e8fb724db
Kimryungyo04 <165171093+Kimryungyo04@users.noreply.github.com>
2024-08-02 00:37:01 +0900

    Update 0802_5_Kimryungyo04.py

--- 시즌10/중/Kimryungyo04/0802_5_Kimryungyo04.py
+++ 시즌10/중/Kimryungyo04/0802_5_Kimryungyo04.py
@@ -1,10 +1,11 @@
+# 등차수열의 합 공식 활용
+
+from math import ceil
 W, H, N = map(int, input().split())
 diff = H / N
-length = 0
 
-for x in range(1, N):
-    height = diff * x
-    if x < (N / 2): length -= height
-    elif x > (N / 2): length += height
-
-print(f"{length * 2:.6f}")
+mid = ceil(N / 2) - 1
+sum_a = mid / 2 * (2 * diff + (mid - 1) * diff)
+sum_b = (H * mid) - sum_a
+length = (sum_b - sum_a) * 2
+print(f"{length:.6f}")
