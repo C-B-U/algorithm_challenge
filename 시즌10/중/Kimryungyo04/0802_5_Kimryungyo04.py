e56fd515636078f9ddd3b4885854cdb6ba6c164c
Kimryungyo04 <165171093+Kimryungyo04@users.noreply.github.com>
2024-08-03 00:13:31 +0900

    Update 0802_5_Kimryungyo04.py

--- 시즌10/중/Kimryungyo04/0802_5_Kimryungyo04.py
+++ 시즌10/중/Kimryungyo04/0802_5_Kimryungyo04.py
@@ -4,7 +4,7 @@ from math import ceil
 W, H, N = map(int, input().split())
 
 mid = ceil(N / 2) - 1
-sum_a = mid / 2 * (2 * diff + (mid - 1) * diff)
+sum_a = mid / 2 * (2 * (H / N) + (mid - 1) * (H / N))
 sum_b = (H * mid) - sum_a
 length = (sum_b - sum_a) * 2
 print(f"{length:.6f}")
