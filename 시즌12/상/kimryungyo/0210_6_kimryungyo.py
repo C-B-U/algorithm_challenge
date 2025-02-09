40283053eb5a97decb1a5045c1afff5dbb25b413
Kimryungyo04 <165171093+kimryungyo@users.noreply.github.com>
2025-02-10 05:28:59 +0900

    Update 0210_6_kimryungyo.py

--- 시즌12/상/kimryungyo/0210_6_kimryungyo.py
+++ 시즌12/상/kimryungyo/0210_6_kimryungyo.py
@@ -1,5 +1,7 @@
-# 1081번 합, 1019번 책 페이지 문제와 유사한 문제
-# 반년 전에 풀었던 코드 제출 
+# [ 자리합 ] - 1081번 합, 1019번 책 페이지 문제와 유사한 문제
+#
+# 숫자들을 자세히 보다 보면 어떤 규칙을 찾을 수 있다
+# 반년 전에 풀었던 문제라 수학적인 증명은 기억나지 않는다..
 
 def unit_and_count(depth):
     counts = {}
@@ -10,16 +12,20 @@ def unit_and_count(depth):
     for depth in range(0, depth):
 
         add_one = 10 ** depth - 1
-        add_default = depth * 10 ** (depth - 1) if depth != 0 else 0
+        add_default = (depth * 10 ** (depth - 1)) if depth != 0 else 0
 
         for num in range(1, 10): 
             distance = [add_default] * 10
             distance[num] += add_one
 
-            if 1 <= num + 1 <= 9: distance[num + 1] += 1
-            else: distance[0] += 1
+            if 1 <= num + 1 <= 9: 
+                distance[num + 1] += 1
+            else: 
+                distance[0] += 1
 
-            if num == 9: distance[1] += 1
+            if num == 9: 
+                distance[1] += 1
+
             count = [count[n] + distance[n] for n in range(10)]
             counts[(num + 1) * 10 ** depth] = count.copy()
     
@@ -39,9 +45,11 @@ def get_number_count(n):
         count = [count[n] + count_dp[influential][n] for n in range(10)]
         
         if (position < length) and (number != 0) and (position > 1):
-            for add in range(1, position): count[0] += int("9" * add)
+            for add in range(1, position): 
+                count[0] += int("9" * add)
 
-        for front in numbers[:idx]: count[front] += number * (10 ** (position - 1))
+        for front in numbers[:idx]: 
+            count[front] += number * (10 ** (position - 1))
 
     return count
 
@@ -49,6 +57,7 @@ n = int(input())
 for _ in range(n):
     a, b = map(int, input().split())
     if a == 0: a = 1
+
     a_c = get_number_count(a-1)
     b_c = get_number_count(b)
 
