9d35e779899519deb01c921cecd455e1e2fa04c9
HongSukYoung <145235252+HongSukYoung@users.noreply.github.com>
2025-04-01 08:33:43 +0900

    Add files via upload

---
+++ 시즌13/중/HongSeokYoung/250331_1_HongSeokYoung.py
@@ -0,0 +1,12 @@
+N = int(input())
+
+dp = [0] * (N + 1)
+
+for i in range(2, N + 1):
+    dp[i] = dp[i - 1] + 1  # 기본적으로 1 빼는 연산
+    if i % 2 == 0:
+        dp[i] = min(dp[i], dp[i // 2] + 1)
+    if i % 3 == 0:
+        dp[i] = min(dp[i], dp[i // 3] + 1)
+
+print(dp[N])
