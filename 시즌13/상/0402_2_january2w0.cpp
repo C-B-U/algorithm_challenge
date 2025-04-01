5f19610fca53743fa2a050296e72a54fc81e2e09
january2w0 <76725495+january2w0@users.noreply.github.com>
2025-04-02 01:49:49 +0900

    Create 0402_2_january2w0.cpp

---
+++ 시즌13/상/0402_2_january2w0.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <map>
+#include <utility>
+#include <numeric>
+
+using namespace std;
+
+int main()
+{
+    int n, max = 0;
+    long xi, yi, ox, oy;
+
+    cin >> ox >> oy >> n;
+
+    map<pair<long, long>, int> slope[2];
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> xi >> yi;
+        if (xi == ox || yi >= oy) continue;
+
+        long p = (xi - ox)*(xi - ox);
+        long q = yi - oy;
+        long g = gcd(p, q);
+
+        int num = ++slope[xi > ox][{ p/g, q/g }];
+        if (num > max) max = num;
+    }
+
+    cout << max << endl;
+
+    return 0;
+}
