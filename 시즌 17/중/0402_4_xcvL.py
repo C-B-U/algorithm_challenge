60795314a811083af20a74ee7480dd97e5044d55
xcvL <baek071106@gmail.com>
2026-04-02 20:08:21 +0900

    0402_4_xcvL

---
+++ 시즌 17/중/0402_4_xcvL.py
@@ -0,0 +1,39 @@
+import sys
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+
+styles = []
+sups = []
+pre_sup = -1
+
+for _ in range(N):
+    style, sup = input().split()
+    sup = int(sup)
+
+    if sup == pre_sup:
+        continue
+
+    styles.append(style)
+    sups.append(sup)
+    pre_sup = sup
+
+def binary_search(target):
+    left, right = 0, len(sups) - 1
+    ans = right
+
+    while left <= right:
+        mid = (left + right) // 2
+
+        if sups[mid] >= target:
+            ans = mid
+            right = mid - 1
+        else:
+            left = mid + 1
+    
+    return ans
+
+for _ in range(M):
+    power = int(input())
+    idx = binary_search(power)
+    print(styles[idx])
