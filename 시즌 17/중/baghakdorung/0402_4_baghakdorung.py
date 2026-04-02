6ca4b488a933031d1caad810fef7dcbc7db20282
baghakdorung <yhak0205@gmail.com>
2026-04-03 00:14:20 +0900

    4일차 제출

---
+++ 시즌 17/중/baghakdorung/0402_4_baghakdorung.py
@@ -0,0 +1,22 @@
+﻿import sys
+from bisect import bisect_left
+
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+
+titles = []
+limits = []
+
+for _ in range(n):
+    title, limit = input().split()
+    limit = int(limit)
+
+    if not limits or limits[-1] != limit:
+        titles.append(title)
+        limits.append(limit)
+
+for _ in range(m):
+    score = int(input())
+    idx = bisect_left(limits, score)
+    print(titles[idx])
