4f0cd8e2c140fe5f89f6a1c668de390c4e4a75d8
Toycartoon <53961972+Toycartoon@users.noreply.github.com>
2026-04-04 19:26:23 +0900

    Delete 시즌 17/중/0331_2_xcvL.py
    파일 위치 문제

--- 시즌 17/중/0331_2_xcvL.py
+++
@@ -1,9 +0,0 @@
-Nlist = []
-T = int(input())
-for _ in range(T):
-    N, M = map(int, input().split())
-    for _ in range(M):
-        input()
-    Nlist.append(N - 1)
-for n in Nlist:
-    print(n)
