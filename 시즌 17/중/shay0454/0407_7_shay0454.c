80188c51c54a91a56af0438d95574f704e079dba
shay0454 <108744131+shay0454@users.noreply.github.com>
2026-04-07 14:53:43 +0900

    Update 0407_7_shay0454.c

--- 시즌 17/중/shay0454/0407_7_shay0454.c
+++ 시즌 17/중/shay0454/0407_7_shay0454.c
@@ -1,13 +1,11 @@
 #include<iostream>
 #include<string>
-#include<queue>
 #include<algorithm>
 
 int main() {
 	std::ios::sync_with_stdio(0); std::cin.tie(0); std::cout.tie(0);
 	std::string S, T;
 	std::cin >> S >> T;
-	std::queue<std::string> q;
 
 	// 1,2 번에 따른 결과인지는 결과의 마지막 문자로 분류가능
 	// 역으로 계산하여 크기가 같을 떄 같은지를 확인
